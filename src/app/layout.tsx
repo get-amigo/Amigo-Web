5aa49b7dfd8cbdd38dc2fa2a6fe06844f21bdc14
Milind Mishra <milind.mishra4@gmail.com>
2024-04-29 15:20:55 +0530

    📦 NEW: add catppuccin theme plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Readex_Pro } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const readexPro = Readex_Pro({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={readexPro.className}>{children}</body>
+      <body className={cn(readexPro.className, "mocha")}>{children}</body>
     </html>
   );
 }
