87fa14c7a244059a04f4f6fe837a0f16b563395b
amit-ksh <ksamit1110@gmail.com>
2024-05-08 17:23:15 +0530

    feat: make hero responsive

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -6,9 +6,9 @@ import { PlayStoreLink } from "../playstore-link";
 
 const Hero = () => {
   return (
-    <div className="relative isolate pt-14">
+    <div className="relative isolate flex justify-center py-16">
       <svg
-        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        className="absolute inset-0 -z-10 h-full w-full stroke-gray-300 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
         aria-hidden="true"
       >
         <defs>
@@ -23,7 +23,7 @@ const Hero = () => {
             <path d="M100 200V.5M.5 .5H200" fill="none" />
           </pattern>
         </defs>
-        <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+        <svg x="50%" y={-1} className="overflow-visible fill-gray-100">
           <path
             d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
             strokeWidth={0}
@@ -36,16 +36,16 @@ const Hero = () => {
           fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
         />
       </svg>
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
-        <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-          <div className="flex">
-            <div className="relative flex items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+      <div className="mx-auto max-w-7xl px-6 py-10 flex flex-col md:flex-row justify-between gap-10 lg:gap-0">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto self-center space-y-10">
+          <div className="flex justify-center md:justify-start">
+            <div className="flex flex-col sm:flex-row items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 ">
               <span className="font-semibold text-purple-600">
                 We&apos;re in public beta
               </span>
-              <span className="h-4 w-px bg-gray-900/10" aria-hidden="true" />
+              <hr className="w-full sm:w-px h-px sm:h-full border-t-0 bg-gray-400" />
               <a href="#" className="flex items-center gap-x-1">
-                <span className="absolute inset-0" aria-hidden="true" />
+                <span className="" aria-hidden="true" />
                 Launch Announcement
                 <ChevronRightIcon
                   className="-mr-2 h-5 w-5 text-gray-400"
@@ -54,23 +54,24 @@ const Hero = () => {
               </a>
             </div>
           </div>
-          <h1 className="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            A better way to split your money
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            We help you manage your expenses and split them.
-          </p>
-          <div className="mt-10 flex items-center gap-x-6">
+          <div className="">
+            <h1 className="text-center md:text-left max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
+              A better way to split your money
+            </h1>
+            <p className="mt-6 text-center md:text-left text-lg leading-8 text-gray-600">
+              We help you manage your expenses and split them.
+            </p>
+          </div>
+          <div className="flex justify-center md:justify-start items-center gap-x-6">
             <AppStoreLink link="https://apps.apple.com/in/app/amigo/id6483936159" />
             <PlayStoreLink link="https://play.google.com/store/apps/details?id=app.amigo.app&hl=en-US" />
-            
           </div>
         </div>
-        <div className="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+        <div className="px-8 w-full md:w-[40%] max-h-[570px]">
           <svg
             viewBox="0 0 366 729"
             role="img"
-            className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl"
+            className="mx-auto w-[22.875rem] max-w-full h-full drop-shadow-xl"
           >
             <title>App screenshot</title>
             <defs>
