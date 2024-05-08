7173cd10251fa718956d8de67feeb0927df57437
amit-ksh <ksamit1110@gmail.com>
2024-05-08 22:04:20 +0530

    feat: text reveal section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,18 @@
 import Nav from "@/components/landing/nav";
 import Hero from "@/components/landing/hero";
 import Footer from "@/components/landing/footer";
+import TextRevealEffect from "@/components/landing/text-reveal-effect";
 
 export default function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <TextRevealEffect
+        className="bg-gray-950"
+        words="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tenetur praesentium ex omnis eos harum non qui minima eveniet laudantium!"
+      />
+
       <Footer />
     </>
   );
