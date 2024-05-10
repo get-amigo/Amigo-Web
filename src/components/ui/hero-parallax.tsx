85d2f31c5c1a2e71f1ed1f245c21f86e2182bf7a
Nanda Lal Das <tikludas01@gmail.com>
2024-05-10 22:02:39 +0530

    changed the Hero parallax d escription

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -105,12 +105,15 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+        The Ultimate <br /> Expense splitting app
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+       Introducing Amigo, your ultimate financial companion revolutionizing the way you manage money. 
+       With Amigo, budgeting becomes a breeze as it effortlessly organizes your expenses and simplifies
+        cost-splitting with friends and roommates. Say goodbye to financial stress and hello to a smarter,
+         more intuitive approach to money management. Seamlessly navigate your financial landscape with Amigo's intuitive features, 
+         empowering you to achieve your financial goals with confidence. Join the Amigo community today and 
+       experience a better way to split your money.
       </p>
     </div>
   );
